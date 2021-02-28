a2bf678f97d782cde453e3709da4dadd6da1063b
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2021-02-28 20:30:17 +0200

    init task with polindrom

--- Polindrom.py
+++ Polindrom.py
@@ -1,34 +1,35 @@
 import math
+import time
+
+start_time = time.time()
 
 
 # Знайти найбільший існуючий поліндром який може утворитися
 # завдяки добутку двох простих пятизначних чисел.
 
 def find_poli():
-    # ----finding prime numbers--------
-    lst = []
-    for num in range(30001, 33500, 2):
-        if all(num % i != 0 for i in range(3, int(math.sqrt(num)) + 1, 2)):
-            lst.append(num)
-
     # ------check if it is a palindrome--------
     def is_polka(numm):
         if all(numm[j] == numm[(len(numm) - 1) - j] for j in range(len(numm))):
             return True
         return False
 
-    # --------list with multiplication of prime number-----------
+    # ----finding prime numbers--------
+    lst = []
+    for num in range(30001, 33500, 2):
+        if all(num % i != 0 for i in range(3, int(math.sqrt(num)) + 1, 2)):
+            lst.append(num)
+
+    # --------list with palindrome-----------
     lfm = []
 
     for x in lst:
         for y in lst:
-            lfm.append(str(x * y))
-    # ------------checking the lis and finding the polyndrome
-    res = 0
-    for k in range(len(lfm)):
-        if is_polka(lfm[k]) and int(lfm[k]) > res:
-            res = int(lfm[k])
-    print(res)
+            if is_polka(str(x * y)):
+                lfm.append(str(x * y))
+    # ------------checking max num of  list
+    print(max(lfm))
 
 
 find_poli()
+print("--- %s seconds ---" % (time.time() - start_time))

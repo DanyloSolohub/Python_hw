3e46c199df7ab8361b1809c1888f8cdb55b7e15f
DanyloSolohub <danylo.solohub.te.2019@lpnu.ua>
2021-03-01 13:44:16 +0200

    initial

--- 1-st hw && cw/Polindrom.py
+++ 1-st hw && cw/Polindrom.py
@@ -8,7 +8,7 @@ start_time = time.time()
 # завдяки добутку двох простих пятизначних чисел.
 
 def find_poli():
-    # ------check if it is a palindrome--------
+    # ------check if num is a palindrome--------
     def is_polka(numm):
         if all(numm[j] == numm[(len(numm) - 1) - j] for j in range(len(numm))):
             return True
@@ -16,19 +16,28 @@ def find_poli():
 
     # ----finding prime numbers--------
     lst = []
-    for num in range(30001, 33500, 2):
+    for num in range(10001, 100000, 2):
         if all(num % i != 0 for i in range(3, int(math.sqrt(num)) + 1, 2)):
             lst.append(num)
 
-    # --------list with palindrome-----------
-    lfm = []
+    # --------res = max palindrome-----------
+    res = 0
+    for x in lst:
+        for y in lst:
+            if is_polka(str(x * y)) and x * y > res:
+                res = x * y
 
+    print(res)
+
+    """
+    lfm = []
     for x in lst:
         for y in lst:
             if is_polka(str(x * y)):
                 lfm.append(str(x * y))
     # ------------checking max num of  list
     print(max(lfm))
+    """
 
 
 find_poli()
